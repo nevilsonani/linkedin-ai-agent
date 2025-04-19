40ac426c40efa1fbc669ed022f425d8c346d7d47
Nevil Sonani <122978643+nevilsonani@users.noreply.github.com>
2025-04-20 00:10:49 +0530

    Delete config directory

--- config/settings.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-LINKEDIN_EMAIL = os.getenv("LINKEDIN_EMAIL")
-LINKEDIN_PASSWORD = os.getenv("LINKEDIN_PASSWORD")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
